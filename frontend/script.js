07ce4db6f233c940ec11cd45503758483c34b538
sagimartin <sagimrtin@gmail.com>
2023-04-12 12:07:02 +0200

    some script.js practice

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+const prim1 = "szoveg" // string
+const prim2 = 10 // integer
+const prim3 = false // boolean
+const prim4 = 0.2 // float
+
+const arr1 = ["kutya", "cica", "mérési hiba"]
+const arr2 = [10, 5, 22, 49]
+const arr3 = [true, false, true]
+
+const obj = {
+    key1: "Ricsi",
+    key2: "Macska",
+    key3: 23,
+    key4: ["alma", "körte", "vegyes"],
+    key5: arr2,
+}
+
+function doubleTheNumber(number) {
+    return number * 2
+}
+
+console.log(doubleTheNumber(7,5));
